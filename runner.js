e836780cf4a773b4e90e61086695611ed6e2b7a3
saif <official.saifmalik@gmail.com>
2021-08-23 18:01:27 +0530

    fix port

--- runner.js
+++ runner.js
@@ -1,8 +1,19 @@
+const http = require('http');
+
 const urlTimer = require("./urlFetch");
 const automateTimer = require("./automate");
 const trashTimer = require("./DatabaseManager/trashCollector");
 
-async function run() {
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('created by saif-malik-01');
+}).listen(process.env.PORT || 5000);
+
+
+
+ function run() {
    urlTimer();
    setTimeout(automateTimer, 5000);
    setTimeout( trashTimer, 10000);
